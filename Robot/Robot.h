9ef776fd02b9dd773add501c87d337c2e88b3402
bencw99 <ben.cohenwang@gmail.com>
2014-11-15 20:58:53 -0800

    CREAED Robot header file

---
+++ Robot/Robot.h
@@ -0,0 +1,19 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+#include <WPILib.h>
+#include "Constants.h"
+#include "DriveTrain.h"
+#include "HumanController.h"
+
+class Robot::public IterativeRobot
+{
+	private:
+		DriveTrain driveTrain;
+		HumanController humanController;
+
+	public:
+		Robot();
+		void transferDriveTrainValues();
+}
+
+#endif
