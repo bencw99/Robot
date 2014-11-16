650d7a7eb330100f174453661966ea314a48a554
bencw99 <ben.cohenwang@gmail.com>
2014-11-15 20:34:46 -0800

    CREATED DriveTrain.cpp

---
+++ Robot/DriveTrain.cpp
@@ -0,0 +1,43 @@
+#include "DriveTrain.h"
+
+DriveTrain::DriveTrain():
+	backLeftVic((uint32_t) PORT_DRIVE_VIC_LEFT_BACK),
+	backRightVic((uint32_t) PORT_DRIVE_VIC_RIGHT_BACK),
+	frontLeftVic((uint32_t) PORT_DRIVE_VIC_LEFT_FRONT),
+	frontRightVic((uint32_t) PORT_DRIVE_VIC_RIGHT_FRONT)
+{
+	stop();
+}
+
+void update()
+{
+	double leftSpeed = min(max(movementSpeed - rotationSpeed, -1), 1);
+	double rightSpeed = min(max(movementSpeed + rotationSpeed, -1), 1);
+	
+	backLeftVic.Set(-leftSpeed);
+	frontLeftVic.Set(-leftSpeed);
+	backRightVic.Set(rightSpeed);
+	frontRightVic.Set(rightSpeed);
+}
+
+void DriveTrain::setMovement(float movementSpeed)
+{
+	this->movementSpeed = movementSpeed;
+}
+
+void DriveTrain::setRotation(float rotationSpeed)
+{
+	this->rotationSpeed = rotationSpeed;
+}
+
+void DriveTrain::stop()
+{
+	setMovement(0);
+	setRotation(0);
+}
+
+void DriveTrain::disable()
+{
+	stop();
+}
+
